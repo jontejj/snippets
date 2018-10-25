5c712c8f7f56b7d7fa2c20078db30751269febca
Jonatan Jönsson <jontejj@gmail.com>
2018-10-25 02:18:23 +0200

    Playground for coarse-parallel-processing-work-queue
    See https://kubernetes.io/docs/tasks/job/coarse-parallel-processing-work-queue/

---
+++ k8/setup.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Depends on minikube / gcloud being started and configured
+cd queue
+kubectl create configmap example-redis-config --from-file=redis-config
+kubectl create -f redis-pod.yaml
+
+#kubectl create -f rabbitmq-service.yaml
+#kubectl create -f rabbitmq-controller.yaml
+
+cd worker
+docker build -t job-wq-1 . -f DockerFile
+docker tag job-wq-1 jontejj/job-wq-1
+docker push jontejj/job-wq-1
+kubectl create -f ./job.yaml
+
+
+#Sidetrack
+docker build . -f DockerFile
+
+#or
+kubectl run -i --tty temp --image ubuntu:14.04
+    apt-get update
+    apt-get install -y curl ca-certificates amqp-tools python dnsutils
